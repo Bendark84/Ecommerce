80e75255b5358f69e247fdcc4aa13d592c4fa8ac
Bendark84 <benrc84@hotmail.com>
2022-06-16 20:12:12 -0600

    cambios 16/02/22

--- assetes/js/main.js
+++ assetes/js/main.js
@@ -48,6 +48,16 @@ let listProducts = document.querySelector(".img-card") /* este es donde esta la 
 let cartContainer = document.querySelector(".cart-list")
 let cartCount = document.querySelector("#cart-count")
 let cart= []
+let contador = 0
+
+
+console.log(window.localStorage.getItem("contador"))
+if(JSON.parse(window.localStorage.getItem("contador") != null)){
+    contador = window.localStorage.getItem("contador")
+    
+}
+
+
 
 /*Callback para mostrar el evento  */
 
@@ -64,6 +74,8 @@ cartIcon.addEventListener("click", () =>{
 cartClose.addEventListener("click", () =>{
     cartOverlay.classList.remove("mostrar")
 })
+
+
 
 
 
@@ -143,12 +155,17 @@ function agregarProducto( producto ){
     }
 
         console.log(cart)
-     
-     mostrarProductosCart()
-}
+
+
+
+        
+        mostrarProductosCart()
+        
+    }
 
 
 /*----------------------MOSTRAR PRODUCTO CART----------------------------------------------- */
+
 
 function mostrarProductosCart(){
 
@@ -159,31 +176,152 @@ function mostrarProductosCart(){
     cart.forEach( item =>{
         fragmentoHTML += `
         <div class="cart-item">
+        
             <img src=${item.image} alt="">
             <p>${item.name}</p>
             <small>Cantidad: ${item.quantitySelected}</small>
+            <button  class="botonMas">+</button>
+            <button>-</button>
+            <button class="buttonDelete">x</button>
         </div>
-        `
+        `     
 
         let totalProducto = item.quantitySelected * item.price 
         suma += totalProducto
 
         cantidadTotal += item.quantitySelected
+       
+
     })
 
     fragmentoHTML += `
     <div class="cart-price">
-        <p> Total de Productos :${ cantidadTotal }</p>
-        <p>$${ suma }</p>
+    
+        <p> Total de Productos  : <br>   ${ cantidadTotal }</p><br>
+        <p>$${ suma }</p><br>
+        <div class="botonesCart">
+            
+        </div>
+       
     </div>
-    `
+    ` 
+ 
+
+
     cartContainer.innerHTML = fragmentoHTML
     cartCount.textContent = cantidadTotal
+
+
 }
+
+// const db = {
+//     items: window.localStorage.getItem('products') ? JSON.parse(window.localStorage.getItem('products')) : items,
+//     methods: {
+//       find: (id) => {
+//         return db.items.find(item => item.id === id)
+//       },
+//       getAll: () => {
+//         return db.items
+//       },
+//       remove: (items) => {
+//         items.forEach(item => {
+//           const product = db.methods.find(item.id)
+//           product.quantity = product.quantity - item.quantity
+//         })
+//       }
+//     }
+//   }
+  
+//   export const renderProducts = () => {
+//     const productsContainer = document.querySelector('#products .products__content')
+//     const products = db.methods.getAll()
+//     let html = ''
+  
+//     products.forEach(product => {
+//       html += `
+//         <article class="products__card ${product.category}">
+//         <div class="products__shape">
+//           <img src="${product.image}" alt="${product.name}" class="products__img">
+//         </div>
+  
+//         <div class="products__data">
+//           <h2 class="products__price">${numberToCurrency(product.price)} <span class="products__quantity">| Stock: ${product.quantity}</span></h2>
+//           <h3 class="products__name">${product.name}</h3>
+  
+//           <button class="button products__button" data-id="${product.id}">
+//             <i class='bx bx-plus'></i>
+//           </button>
+//         </div>
+//         </article>`
+//     })
+  
+//     productsContainer.innerHTML += html
+  
+//     const productsButton = document.querySelectorAll('.products__button')
+  
+//     productsButton.forEach(button => {
+//       button.addEventListener('click', () => {
+//         const id = parseInt(button.getAttribute('data-id'))
+//         const product = db.methods.find(id)
+  
+//         if (product && product.quantity > 0) {
+//           cart.methods.add(id, 1)
+//           renderCart()
+//         } else {
+//           window.alert('Sorry, we are out of stock')
+//         }
+//       })
+//     })
+  
+//     window.localStorage.setItem('products', JSON.stringify(db.items))
+//   }
+
+
+
+
+
+
+
+
+// function activeProduct () {
+//     const linksProducts = document.querySelectorAll('.products__item')
+  
+//     for (let i = 0; i < linksProducts.length; i++) {
+//       linksProducts[i].addEventListener('click', function () {
+//         for (let j = 0; j < linksProducts.length; j++) {
+//           linksProducts[j].classList.remove('active-product')
+//         }
+//         this.classList.add('active-product')
+//       })
+//     }
+//   }
+
+
+// //Quantity Valor
+// /let quantityValues = document.getElementsByClassName("cart-quantity")
+// for (let i=0; i<quantityValues.length; i++){
+//     let value = quantityValues[i]
+//     value.addEventListener ("change", quantityChanged)
+// }
+
+// function quantityChanged (event){
+//     let input = event.target
+//     if( input.value == 0 || input.value == NaN){
+//         input.value = 1
+//     }
+//     updateTotal();
+// }
+// Reaccionar
+// Responder
+
+// 18:15
+// mi updatetotal
+
+
 
 /* Local Storage */
 
-
+/*window.localStorage*/
 
 
 
