902ebb63f5f677d9502cdd659a41f7e11941c4bb
Bendark84 <benrc84@hotmail.com>
2022-06-16 22:35:05 -0600

    localStorege actualizado

--- assetes/js/main.js
+++ assetes/js/main.js
@@ -51,18 +51,26 @@ let cart= []
 let contador = 0
 
 
-console.log(window.localStorage.getItem("contador"))
-if(JSON.parse(window.localStorage.getItem("contador") != null)){
-    contador = window.localStorage.getItem("contador")
+
+// if(JSON.parse(window.localStorage.getItem("contador") != null)){
+//     contador = window.localStorage.getItem("contador")
     
-}
+// }
 
 
 
 /*Callback para mostrar el evento  */
 
 document.addEventListener("DOMContentLoaded" , () =>{
+    
+    if(window.localStorage.getItem("cart")){
+        cart=JSON.parse(window.localStorage.getItem("cart"))
+    }else{
+        window.localStorage.setItem("cart", JSON.stringify(cart))
+    }
+    
     mostrarProductos()
+    mostrarProductosCart()
 
 })
 
@@ -121,10 +129,6 @@ function mostrarProductos(){
     })
 }
 
-// function nuevoProducto(){
-//     console.log("nuevo producto")
-    
-//}
 
 
 
@@ -154,9 +158,9 @@ function agregarProducto( producto ){
         cart.push(producto)
     }
 
-        console.log(cart)
+        
 
-
+    localStorage.setItem("cart",JSON.stringify(cart))
 
         
         mostrarProductosCart()
@@ -165,6 +169,9 @@ function agregarProducto( producto ){
 
 
 /*----------------------MOSTRAR PRODUCTO CART----------------------------------------------- */
+
+
+
 
 
 function mostrarProductosCart(){
@@ -205,6 +212,8 @@ function mostrarProductosCart(){
        
     </div>
     ` 
+    
+
  
 
 
@@ -214,108 +223,7 @@ function mostrarProductosCart(){
 
 }
 
-// const db = {
-//     items: window.localStorage.getItem('products') ? JSON.parse(window.localStorage.getItem('products')) : items,
-//     methods: {
-//       find: (id) => {
-//         return db.items.find(item => item.id === id)
-//       },
-//       getAll: () => {
-//         return db.items
-//       },
-//       remove: (items) => {
-//         items.forEach(item => {
-//           const product = db.methods.find(item.id)
-//           product.quantity = product.quantity - item.quantity
-//         })
-//       }
-//     }
-//   }
-  
-//   export const renderProducts = () => {
-//     const productsContainer = document.querySelector('#products .products__content')
-//     const products = db.methods.getAll()
-//     let html = ''
-  
-//     products.forEach(product => {
-//       html += `
-//         <article class="products__card ${product.category}">
-//         <div class="products__shape">
-//           <img src="${product.image}" alt="${product.name}" class="products__img">
-//         </div>
-  
-//         <div class="products__data">
-//           <h2 class="products__price">${numberToCurrency(product.price)} <span class="products__quantity">| Stock: ${product.quantity}</span></h2>
-//           <h3 class="products__name">${product.name}</h3>
-  
-//           <button class="button products__button" data-id="${product.id}">
-//             <i class='bx bx-plus'></i>
-//           </button>
-//         </div>
-//         </article>`
-//     })
-  
-//     productsContainer.innerHTML += html
-  
-//     const productsButton = document.querySelectorAll('.products__button')
-  
-//     productsButton.forEach(button => {
-//       button.addEventListener('click', () => {
-//         const id = parseInt(button.getAttribute('data-id'))
-//         const product = db.methods.find(id)
-  
-//         if (product && product.quantity > 0) {
-//           cart.methods.add(id, 1)
-//           renderCart()
-//         } else {
-//           window.alert('Sorry, we are out of stock')
-//         }
-//       })
-//     })
-  
-//     window.localStorage.setItem('products', JSON.stringify(db.items))
-//   }
 
-
-
-
-
-
-
-
-// function activeProduct () {
-//     const linksProducts = document.querySelectorAll('.products__item')
-  
-//     for (let i = 0; i < linksProducts.length; i++) {
-//       linksProducts[i].addEventListener('click', function () {
-//         for (let j = 0; j < linksProducts.length; j++) {
-//           linksProducts[j].classList.remove('active-product')
-//         }
-//         this.classList.add('active-product')
-//       })
-//     }
-//   }
-
-
-// //Quantity Valor
-// /let quantityValues = document.getElementsByClassName("cart-quantity")
-// for (let i=0; i<quantityValues.length; i++){
-//     let value = quantityValues[i]
-//     value.addEventListener ("change", quantityChanged)
-// }
-
-// function quantityChanged (event){
-//     let input = event.target
-//     if( input.value == 0 || input.value == NaN){
-//         input.value = 1
-//     }
-//     updateTotal();
-// }
-// Reaccionar
-// Responder
-
-// 18:15
-// mi updatetotal
 
 
 
